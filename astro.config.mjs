4a57642f5fafcd4438bf7fa794236693356085a1
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-06-29 02:59:05 +0900

    [Update] base削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,9 @@ import { loadEnv } from 'vite';
 const env = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 const site =
   env.TARGET === 'gh-pages' ? 'https://inxst.github.io' : 'https://hoge.com';
-const base = env.TARGET === 'gh-pages' ? '/st-portfolio' : '';
 
 // https://astro.build/config
 export default defineConfig({
   site: site,
-  base: base,
   integrations: [tailwind()],
 });
