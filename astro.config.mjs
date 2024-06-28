8c0995595eea236448f3da0613dc95f98ecfdc66
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-06-29 02:36:32 +0900

    [Update] deploy設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { loadEnv } from 'vite';
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+const site =
+  env.TARGET === 'gh-pages' ? 'https://inxst.github.io' : 'https://hoge.com';
+const base = env.TARGET === 'gh-pages' ? '/st-portfolio' : '';
 
 // https://astro.build/config
 export default defineConfig({
+  site: site,
+  base: base,
   integrations: [tailwind()],
 });
