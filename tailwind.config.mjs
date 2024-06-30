228a8bd55d48633a8d9f3cc5986be01b5c5816a5
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-06-30 23:18:31 +0900

    [Add] container設定

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,13 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        default: '1360px',
+      },
+    },
     extend: {
       fontFamily: {
         serif: ['Zen Old Mincho', ...defaultTheme.fontFamily.serif],
