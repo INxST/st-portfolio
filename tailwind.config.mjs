a73438dab50148f71d09f6acb1217be972ac987d
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-06-30 23:10:26 +0900

    [Add] font 設定

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['Zen Old Mincho', ...defaultTheme.fontFamily.serif],
+        'serif-en': ['Marcellus', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 };
