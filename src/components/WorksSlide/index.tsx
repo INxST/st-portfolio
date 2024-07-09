887bd6caee749b828225f5f891a1a08fcc32a207
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-07-10 02:53:51 +0900

    [Add] filter実装, slide調整

--- src/components/WorksSlide/index.tsx
+++ src/components/WorksSlide/index.tsx
@@ -1,6 +1,7 @@
 import type { Work } from '@/types/Work';
-import React, { useLayoutEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import scrollSlide from './scrollSlide';
+import filters from '@/data/filters';
 
 type Props = {
   children: React.ReactNode;
@@ -8,68 +9,116 @@ type Props = {
 };
 
 const WorksSlide = ({ children, items }: Props) => {
-  useLayoutEffect(() => {
-    scrollSlide();
-  }, []);
+  const [selectedOption, setSelectedOption] = useState('All');
+  const wrapper = useRef<HTMLDivElement | null>(null);
+  const container = useRef<HTMLUListElement | null>(null);
+  const progress = useRef<HTMLDivElement | null>(null);
+
+  const handleOptionChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setSelectedOption(event.target.value);
+  };
+
+  scrollSlide({ wrapper, container, progress });
 
   return (
-    <section
-      id="scroll-slide"
-      className="pt-[var(--header-height)] overflow-x-hidden"
-    >
-      <ul
-        id="scroll-slide-container"
-        className="flex flex-nowrap justify-start whitespace-nowrap"
+    <section>
+      <div
+        ref={wrapper}
+        className="pt-[var(--header-height)]"
       >
-        <li className="ts-scroll-slide-item whitespace-nowrap">{children}</li>
-        {items.map(item => {
-          return (
-            <li
-              key={item.href}
-              className="ts-scroll-slide-item md:w-[calc(calc(590/16)*1rem)] pt-5 pb-6 border-l peer-last:border-r last-of-type:border-r border-silver bg-quill-gray bg-[url(/texture-for-gray.png)] bg-repeat px-12 whitespace-nowrap"
-            >
-              <a href={item.href} className="flex flex-col h-full">
-                <div className="flex flex-1">
-                  <picture className="flex-1 w-[calc(calc(247/14)*1rem)] md:w-[calc(calc(464/16)*1rem)] h-[calc(calc(368/14)*1rem)] md:h-[calc(calc(560/16)*1rem)]">
-                    <img
-                      src={item.image}
-                      alt={item.title}
-                      className="w-full h-full object-cover"
-                    />
-                  </picture>
-                  <div className="vertical-rl flex font-serif-en ml-3">
-                    <ul className="flex flex-wrap gap-4">
-                      {item.categories.map(category => {
-                        return <li>{category}</li>;
+        <ul
+          ref={container}
+          className="flex flex-nowrap justify-start whitespace-nowrap"
+        >
+          <li className="ts-scroll-slide-item whitespace-nowrap">{children}</li>
+          {items.map((item, i) => {
+            return (
+              <li
+                key={`${item.href}-${i}`}
+                className="ts-scroll-slide-item md:w-[calc(calc(590/16)*1rem)] pt-5 pb-6 border-l peer-last:border-r last-of-type:border-r border-silver bg-quill-gray bg-[url(/texture-for-gray.png)] bg-repeat px-12 whitespace-nowrap transition-all duration-500 animate-text-focus-in data-[hidden]:animate-text-blur-out data-[hidden]:w-0"
+                data-tags={item.tags.join(' ')}
+                data-hidden={
+                  selectedOption !== 'All' &&
+                    !item.tags.includes(selectedOption)
+                    ? true
+                    : null
+                }
+              >
+                <a href={item.href} className="flex flex-col h-full">
+                  <div className="flex flex-1">
+                    <picture className="flex-1 w-[calc(calc(247/14)*1rem)] md:w-[calc(calc(464/16)*1rem)] h-[calc(calc(368/14)*1rem)] md:h-[calc(calc(560/16)*1rem)]">
+                      <img
+                        src={item.image}
+                        alt={item.title}
+                        className="w-full h-full object-cover"
+                      />
+                    </picture>
+                    <div className="vertical-rl flex font-serif-en ml-3">
+                      <ul className="flex flex-wrap gap-4">
+                        {item.categories.map((category, i) => {
+                          return <li key={`${item.href}-${category}-${i}`}>{category}</li>;
+                        })}
+                      </ul>
+                      <span className="inline-block mt-auto">{item.year}</span>
+                    </div>
+                  </div>
+                  <div className="mt-4">
+                    <h2 className="font-semibold text-xl md:text-3xl">
+                      {item.title}
+                    </h2>
+                    <ul className="flex flex-wrap gap-x-4 gap-y-2 font-serif-en mt-6 text-gray">
+                      {item.tags.map((tag, i) => {
+                        return <li key={`${item.href}-${tag}-${i}`}>{tag}</li>;
                       })}
                     </ul>
-                    <span className="inline-block mt-auto">{item.year}</span>
                   </div>
-                </div>
-                <div className="mt-4">
-                  <h2 className="font-semibold text-xl md:text-3xl">
-                    {item.title}
-                  </h2>
-                  <ul className="flex flex-wrap gap-x-4 gap-y-2 font-serif-en mt-6 text-gray">
-                    {item.tags.map(tag => {
-                      return <li>{tag}</li>;
-                    })}
-                  </ul>
-                </div>
-              </a>
-            </li>
-          );
-        })}
-        <li className="ts-scroll-slide-item pr-10 flex items-center">
-          <a
-            href="/about"
-            className="ts-text-link text-2xl md:text-[2rem] vertical-rl font-semibold ml-24 md:ml-72 border-r pr-2"
-            data-vertical="rl"
-          >
-            <span>私について</span>
-          </a>
-        </li>
-      </ul>
+                </a>
+              </li>
+            );
+          })}
+          <li className="ts-scroll-slide-item pr-10 flex items-center">
+            <a
+              href="/about"
+              className="ts-text-link text-2xl md:text-[2rem] vertical-rl font-semibold ml-24 md:ml-72 border-r pr-2"
+              data-vertical="rl"
+            >
+              <span>私について</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="fixed bottom-0 left-0 w-screen">
+        <div className="w-full h-[1px] bg-silver">
+          <div className="w-0 h-full bg-black" ref={progress} />
+        </div>
+
+        <form className="flex container px-5 md:px-20 py-[0.875rem] md:py-7 overflow-x-hidden">
+          <p className="font-serif-en text-[1.375rem]">Filter</p>
+          <ul className="flex flex-wrap ml-[7.5rem] gap-4 md:gap-6">
+            {filters.map((filter, i) => {
+              return (
+                <li key={`${filter}-${i}`}>
+                  <label className="font-serif-en flex align-baseline">
+                    <input
+                      className="block box-content m-0 p-0 appearance-none border border-black rounded-full w-4 md:w-5 h-4 md:h-5 relative before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:rounded-full before:w-3 md:before:w-[0.875rem] before:h-3 md:before:h-[0.875rem] checked:before:bg-black"
+                      type="radio"
+                      value={filter}
+                      checked={selectedOption === filter}
+                      onChange={handleOptionChange}
+                    />
+                    <span className="md:text-[1.125rem] ml-2 flex-1">
+                      {filter}
+                    </span>
+                  </label>
+                </li>
+              );
+            })}
+          </ul>
+        </form>
+      </div>
     </section>
   );
 };
--- src/components/WorksSlide/scrollSlide.ts
+++ src/components/WorksSlide/scrollSlide.ts
@@ -1,26 +1,43 @@
+import { useRef, useLayoutEffect } from 'react';
+import type { MutableRefObject } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-const scrollSlide = () => {
-  gsap.registerPlugin(ScrollTrigger);
-  const section = document.getElementById('scroll-slide');
-  const container = document.getElementById('scroll-slide-container');
-  const slides = gsap.utils.toArray<HTMLElement>('.ts-scroll-slide-item');
-  const width = container?.offsetWidth;
+type Args = {
+  wrapper: MutableRefObject<HTMLElement | null>;
+  container: MutableRefObject<HTMLElement | null>;
+  progress: MutableRefObject<HTMLSpanElement | null>;
+};
 
-  gsap.to(container, {
-    xPercent: -100 * (slides.length - 1),
-    ease: 'none',
-    scrollTrigger: {
-      trigger: section,
-      pin: true,
-      scrub: 1,
-      start: 'top top',
-      end: `+=${width}`,
-      anticipatePin: 1,
-      invalidateOnRefresh: true,
-    },
-  });
+const scrollSlide = ({ wrapper, container, progress }: Args) => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const didEffect = useRef(false);
+
+  useLayoutEffect(() => {
+    if (!didEffect.current) {
+      didEffect.current = true;
+
+      const slides = gsap.utils.toArray<HTMLElement>('.ts-scroll-slide-item');
+      const width = container.current?.offsetWidth;
+      gsap.to(container.current, {
+        xPercent: -100 * (slides.length - 1),
+        ease: 'none',
+        scrollTrigger: {
+          trigger: wrapper.current,
+          pin: true,
+          scrub: 1,
+          start: 'top top',
+          end: `+=${width}`,
+          anticipatePin: 1,
+          invalidateOnRefresh: true,
+          onUpdate: self => {
+            progress.current!.style.width = `${(self.progress * 100).toFixed(2)}%`;
+          },
+        },
+      });
+    }
+  }, []);
 };
 
 export default scrollSlide;
