c10f9e76e7a29736e390979302d97376121896c0
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-07-13 00:02:05 +0900

    [Update] sp filter

--- src/components/WorksSlide/index.tsx
+++ src/components/WorksSlide/index.tsx
@@ -10,6 +10,7 @@ type Props = {
 
 const WorksSlide = ({ children, items }: Props) => {
   const [selectedOption, setSelectedOption] = useState('All');
+  const [open, setOpen] = useState(false);
   const wrapper = useRef<HTMLDivElement | null>(null);
   const container = useRef<HTMLUListElement | null>(null);
   const progress = useRef<HTMLDivElement | null>(null);
@@ -28,7 +29,7 @@ const WorksSlide = ({ children, items }: Props) => {
       <div ref={wrapper} className="pt-[var(--header-height)]">
         <ul
           ref={container}
-          className="flex flex-nowrap justify-start whitespace-nowrap h-[calc(100dvh-calc(var(--header-height)+var(--filter-height)))] pt-11 pb-20"
+          className="flex flex-nowrap justify-start whitespace-nowrap h-[calc(100dvh-calc(var(--header-height)+3.25rem))] md:h-[calc(100dvh-calc(var(--header-height)+var(--filter-height)))] pt-11 pb-20"
         >
           <li className="ts-scroll-slide-item">{children}</li>
           {items.map((item, i) => {
@@ -92,18 +93,48 @@ const WorksSlide = ({ children, items }: Props) => {
         </ul>
       </div>
 
-      <div className="fixed bottom-0 left-0 w-screen" ref={filter}>
+      <div
+        className="fixed bottom-0 left-0 w-screen group data-[open]:z-30"
+        data-open={open ? open : null}
+        ref={filter}
+      >
         <div className="w-full h-[1px] bg-silver">
           <div className="w-0 h-full bg-black" ref={progress} />
         </div>
 
-        <form className="flex container px-5 md:px-20 py-[0.875rem] md:py-7 overflow-x-hidden">
-          <p className="font-serif-en text-[1.375rem]">Filter</p>
-          <ul className="flex flex-wrap ml-[7.5rem] gap-4 md:gap-6">
+        <div className="md:hidden transition-all duration-500 fixed top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.5)] opacity-0 invisible group-data-[open]:opacity-100 group-data-[open]:visible" />
+
+        <form className="flex items-start container px-5 md:px-20 py-[0.875rem] md:py-7 overflow-hidden bg-quill-gray bg-[url(/texture-for-gray.png)] bg-repeat relative group-data-[open]:z-40">
+          <button
+            type="button"
+            className="font-serif-en text-[1.375rem] leading-none flex items-center md:pointer-events-none"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            Filter
+            <svg
+              width="12"
+              height="8"
+              viewBox="0 0 12 8"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="fill-current ml-3 md:hidden transition-transform duration-500 group-data-[open]:-rotate-[540deg]"
+            >
+              <path d="M5.90872 7.43347L-0.183594 1.78197L0.907946 0.769409L5.90872 5.43189L10.9095 0.792959L12.001 1.80552L5.90872 7.43347Z" />
+            </svg>
+          </button>
+          <ul className="flex flex-wrap items-center md:items-baseline ml-[7.5rem] gap-4 md:gap-6">
             {filters.map((filter, i) => {
               return (
-                <li key={`${filter}-${i}`}>
-                  <label className="font-serif-en flex align-baseline">
+                <li
+                  key={`${filter}-${i}`}
+                  data-checked={selectedOption === filter ? true : null}
+                  className="transition-all w-0 h-0 opacity-0 invisible animate-text-blur-out md:w-auto md:h-auto md:opacity-100 md:visible md:animate-none 
+                  data-[checked]:w-auto data-[checked]:h-auto data-[checked]:visible data-[checked]:opacity-100 data-[checked]:animate-text-focus-in
+                  group-data-[open]:w-auto group-data-[open]:h-auto group-data-[open]:opacity-100 group-data-[open]:visible group-data-[open]:animate-text-focus-in"
+                >
+                  <label className="font-serif-en flex items-center">
                     <input
                       className="block box-content m-0 p-0 appearance-none border border-black rounded-full w-4 md:w-5 h-4 md:h-5 relative before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:rounded-full before:w-3 md:before:w-[0.875rem] before:h-3 md:before:h-[0.875rem] checked:before:bg-black"
                       type="radio"
