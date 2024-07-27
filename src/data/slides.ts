a03f1b45aaafd1da87b42a7c9c8f64cd7086c716
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-07-28 01:02:57 +0900

    [Update] スクロール画像拡大 全画面に変更

--- src/data/slides.ts
+++ src/data/slides.ts
@@ -3,22 +3,34 @@ import type { Slide } from '@/types/Slide';
 const slides: Slide[] = [
   {
     type: 'image',
-    src: 'https://placehold.jp/3d4070/ffffff/840x473.png',
+    src: {
+      pc: 'https://placehold.jp/3d4070/ffffff/840x473.png',
+      sp: 'https://placehold.jp/3d4070/ffffff/375x667.png',
+    },
     alt: 'image 1',
   },
   {
     type: 'image',
-    src: 'https://placehold.jp/7e1b1b/ffffff/840x673.png',
+    src: {
+      pc: 'https://placehold.jp/3d4070/ffffff/840x473.png',
+      sp: 'https://placehold.jp/3d4070/ffffff/375x667.png',
+    },
     alt: 'image 2',
   },
   {
     type: 'image',
-    src: 'https://placehold.jp/000000/ffffff/840x473.png',
+    src: {
+      pc: 'https://placehold.jp/3d4070/ffffff/840x473.png',
+      sp: 'https://placehold.jp/3d4070/ffffff/375x667.png',
+    },
     alt: 'image 3',
   },
   {
     type: 'video',
-    src: '/sample-5s.mp4',
+    src: {
+      pc: '/sample-5s.mp4',
+      sp: '/sample-5s.mp4',
+    },
   },
 ];
 
