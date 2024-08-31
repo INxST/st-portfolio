2bba5f91ac9374b8cd9756c733ff5089da07ba47
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-08-31 19:03:28 +0900

    🐞 fix: コンテンツ幅調整

--- src/components/TitleVertical.tsx
+++ src/components/TitleVertical.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 const TitleVertical = ({ jp, en }: Props) => {
   return (
-    <h1 className="pt-16 md:pt-32">
+    <h1 className="pt-16 md:pt-32 px-[1.4rem] md:px-0">
       <span className="text-4xl md:text-5xl font-medium leading-none vertical-rl align-top -mt-5">
         <span>{jp}</span>
       </span>
