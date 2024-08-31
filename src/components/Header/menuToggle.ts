cc93f0850a25230d03d7ea05d0f88d1c86d78583
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-09-01 01:01:37 +0900

    🐞 fix: ローディング・ナビ展開時スクロール無効化

--- src/components/Header/menuToggle.ts
+++ src/components/Header/menuToggle.ts
@@ -2,14 +2,18 @@ const menuToggle = () => {
   const open = document.querySelector('.ts-menu-open');
   const close = document.querySelector('.ts-menu-close');
   const menu = document.getElementById('menu');
+  const body = document.body;
   const classOpen = 'is-open';
+  const classMenuOpen = 'is-menu-open';
 
   open?.addEventListener('click', () => {
     menu?.classList.add(classOpen);
+    body.classList.add(classMenuOpen);
   });
 
   close?.addEventListener('click', () => {
     menu?.classList.remove(classOpen);
+    body.classList.remove(classMenuOpen);
   });
 };
 
